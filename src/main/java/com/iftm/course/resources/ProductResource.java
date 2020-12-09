782b1c6e735d9c2d5f53edbbfbea8eb7f9cfe820
Gledson Junior <49466350+GJotaJunior@users.noreply.github.com>
2020-12-08 22:46:24 -0300

    Aula 18
    - Challenge: CategoryDTO, ProductDTO
    - OrderDTO
    - Refactor: no more Jackson in entities
    - Product insert
    - Product update

--- src/main/java/com/iftm/course/resources/ProductResource.java
+++ src/main/java/com/iftm/course/resources/ProductResource.java
@@ -1,15 +1,21 @@
 package com.iftm.course.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import com.iftm.course.entities.Product;
+import com.iftm.course.dto.ProductCategoriesDTO;
+import com.iftm.course.dto.ProductDTO;
 import com.iftm.course.services.ProductService;
 
 @RestController
@@ -20,15 +26,29 @@ public class ProductResource {
 	private ProductService service;
 
 	@GetMapping
-	public ResponseEntity<List<Product>> findAll() {	
-		List<Product> list = service.findAll();
+	public ResponseEntity<List<ProductDTO>> findAll() {	
+		List<ProductDTO> list = service.findAll();
 		return ResponseEntity.ok().body(list);
 	}
 	
 	@GetMapping(value = "/{id}")
-	public ResponseEntity<Product> findById(@PathVariable Long id) {
-		Product obj = service.findById(id);
-		return ResponseEntity.ok().body(obj);
+	public ResponseEntity<ProductDTO> findById(@PathVariable Long id) {
+		ProductDTO dto = service.findById(id);
+		return ResponseEntity.ok().body(dto);
+	}
+	
+	@PostMapping
+	public ResponseEntity<ProductDTO> insert(@RequestBody ProductCategoriesDTO dto) {
+		ProductDTO newDto = service.insert(dto);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+					.buildAndExpand(newDto.getId()).toUri();
+		return ResponseEntity.created(uri).body(newDto);
+	}
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<ProductDTO> update(@PathVariable Long id, @RequestBody ProductCategoriesDTO dto) {
+		ProductDTO newDto = service.update(id, dto);
+		return ResponseEntity.ok().body(newDto);
 	}
 	
 }
--- src/main/java/com/iftm/course/services/ProductService.java
+++ src/main/java/com/iftm/course/services/ProductService.java
@@ -2,12 +2,22 @@ package com.iftm.course.services;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
+
+import javax.persistence.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.iftm.course.dto.CategoryDTO;
+import com.iftm.course.dto.ProductCategoriesDTO;
+import com.iftm.course.dto.ProductDTO;
+import com.iftm.course.entities.Category;
 import com.iftm.course.entities.Product;
+import com.iftm.course.repositories.CategoryRepository;
 import com.iftm.course.repositories.ProductRepository;
+import com.iftm.course.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class ProductService {
@@ -15,13 +25,56 @@ public class ProductService {
 	@Autowired
 	private ProductRepository repository;
 	
-	public List<Product> findAll() {
-		return repository.findAll();
+	@Autowired
+	private CategoryRepository categoryRepository;
+	
+	public List<ProductDTO> findAll() {
+		List<Product> list =  repository.findAll();
+		return list.stream().map(e -> new ProductDTO(e)).collect(Collectors.toList());
 	}
 	
-	public Product findById(Long id) {
+	public ProductDTO findById(Long id) {
 		Optional<Product> obj = repository.findById(id);
-		return obj.get();
+		Product entity = obj.orElseThrow(() -> new ResourceNotFoundException(id));
+		return new ProductDTO(entity);
+	}
+	
+	@Transactional
+	public ProductDTO insert(ProductCategoriesDTO dto) {
+		Product entity = dto.toEntity();
+		setProductCategories(entity, dto.getCategories());
+		entity = repository.save(entity);
+		return new ProductDTO(entity);
+	}
+	
+	@Transactional
+	public ProductDTO update(Long id, ProductCategoriesDTO dto) {
+		try {
+			Product entity = repository.getOne(id);
+			updateData(entity, dto);
+			entity = repository.save(entity);
+			return new ProductDTO(entity);
+		} catch(EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
+	}
+
+	private void updateData(Product entity, ProductCategoriesDTO dto) {
+		entity.setName(dto.getName());
+		entity.setDescription(dto.getDescription());
+		entity.setPrice(dto.getPrice());
+		entity.setImgUrl(dto.getImgUrl());
+		if (dto.getCategories() != null && dto.getCategories().size() > 0) {
+			setProductCategories(entity, dto.getCategories());
+		}
+	}
+	
+	private void setProductCategories(Product entity, List<CategoryDTO> categories) {
+		entity.getCategories().clear();
+		for (CategoryDTO dto : categories) {
+			Category category = categoryRepository.getOne(dto.getId());
+			entity.getCategories().add(category);
+		}
 	}
 
 }
